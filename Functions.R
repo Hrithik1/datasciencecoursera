950afde1e603e6e737700e2544b161106e6e1f36
Hrithik Rana <hrithik2769168@gmail.com>
2020-07-19 20:52:28 +0530

    Functions learning (R)

--- Functions.R
+++ Functions.R
@@ -26,5 +26,39 @@ f_to_c<-function(f){
   return(c)
 }
 
+c_to_k<-function(c){
+  k<-c+273.15
+  return(k)
+}
 
+f_to_k<-function(f)
+{
+  c<-f_to_c(f)
+  k<-c_to_k(c)
+  return(k)
+}
 
+powers<-function(n){
+  pow<-function(x){
+    x^n
+  }
+  pow
+}
+
+y<-10
+f<-function(x){
+  y<-2
+  y^2+g(x)
+}
+g<-function(x){
+  x*y
+}
+
+x<-30
+f<-function(y){
+  x<-20
+  x*y+g(y)
+}
+g<-function(y){
+  x^y
+}
